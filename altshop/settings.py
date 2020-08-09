7083bee5ad5d8b164f60d37cf83f2725200f70ca
Watashi <doni.des.ni@gmail.com>
2020-08-09 13:46:33 -0700

    debug true

--- altshop/settings.py
+++ altshop/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "Doni@Watashi12DesDoni@Watashi12Des"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://calm-taiga-28533.herokuapp.com/']
 
