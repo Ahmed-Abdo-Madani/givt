cd08b70b7d3779ba2640a49381e50cbd7a20eabc
Watashi <doni.des.ni@gmail.com>
2020-09-07 20:43:28 -0700

    details page ready

--- shop/views.py
+++ shop/views.py
@@ -20,7 +20,8 @@ def product_list(request, category_slug=None):
 def product_details(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
     cart_product_form = CartAddProductForm()
-    return render(request, 'shop/product/detailsNew.html', {'product': product, 'cart_product_form': cart_product_form})
+    products = Product.objects.filter(available=True)
+    return render(request, 'shop/product/detailsNew.html', {'product': product, 'products': products, 'cart_product_form': cart_product_form})
 
 
 def index(request):
